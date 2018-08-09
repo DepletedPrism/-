626508fcbe56204c156d50ab12621ae28be06654
DepletedPrism <DepletedPrism@qq.com>
2018-08-09 23:22:09 +0800

    Update Running_RedEnvelope.java

--- Running_RedEnvelope.java
+++ Running_RedEnvelope.java
@@ -13,7 +13,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = Running_RedEnvelope.MODID, name = Running_RedEnvelope.NAME, version = Running_RedEnvelope.VERSION)
 public class Running_RedEnvelope 
 {
-	public static final String MODID = "running_redenvelope";
+    public static final String MODID = "running_redenvelope";
     public static final String NAME = "Running Red Envelope";
     public static final String VERSION = "0.1.0";
     
